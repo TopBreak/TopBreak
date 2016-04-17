09da186f3aeeaec42343e7f9363b9f2a688ca07f
breakLin <1428568656@qq.com>
2016-04-17 14:56:55 +0800

    Update demo.java

--- Test/src/test1/demo.java
+++ Test/src/test1/demo.java
@@ -1,12 +1,9 @@
 package test1;
 /**
  * @author break
- * @createDate 2016-4-17 下午2:34:40
+ * @createDate 2016-4-17 涓嬪崍2:34:40
  * @desc  TODO
- * @ svn 提交者 
- * @svn 提供者  $author$
- * @提交时间  $date$ 
- * @当前版本  $Rev$
+
  */
 public class demo {
 	public static void main(String[] args) {
